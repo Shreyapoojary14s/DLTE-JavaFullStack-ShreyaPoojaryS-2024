903c5991feb8a289325828650c2ba8be51a9acad
Shreyapoojary14s <siripoojary@gmail.com>
2024-02-27 19:46:38 +0530

    Report

--- DLTE-Java/DLTE-Java02/src/main/java/block/service/InsuranceProvider.java
+++ DLTE-Java/DLTE-Java02/src/main/java/block/service/InsuranceProvider.java
@@ -3,36 +3,37 @@ package block.service;
 import java.util.Scanner;
 
 public class InsuranceProvider {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Insurance suggester
-        String need="";
-        Scanner scanner=new Scanner(System.in);
+        String need = "";
+        Scanner scanner = new Scanner(System.in);
         //store in array
-        String[] starHealth={"Accidental","NaturalCalamities","Unfortunate death"};
-        String[] icicLife={"Marriage","Health"};
-        String[] myInsurance={"Property_damage","Vehicle_Insurance"};
+        String[] starHealth = {"Accidental", "NaturalCalamities", "Unfortunate death"};
+        String[] icicLife = {"Marriage", "Health"};
+        String[] myInsurance = {"Property_damage", "Vehicle_Insurance"};
 
-        System.out.println("There are insurance services like Health ,Accidental,Marriage, NaturalCalamities,Unfortunate death,Property_damage,Vehicle_Insurance");
-        System.out.println("----Choose the required insurance ,we are here to suggest you-----");
-        need=scanner.nextLine();
+        //System.out.println("There are insurance services like Health ,Accidental,Marriage, NaturalCalamities,Unfortunate death,Property_damage,Vehicle_Insurance");
+        // System.out.println("----Choose the required insurance ,we are here to suggest you-----");
+        //  need=scanner.nextLine();
 
-       for(int integer=0;integer<starHealth.length;integer++) {
-
-           if (need.equals(starHealth[integer])) {
-               System.out.println("You are suggested to have a Star Health Insurance where it includes " + need + "\tinsurance");
-
-           }
-       }
-        for(int integer=0;integer<icicLife.length;integer++){
-            if(need.equals(icicLife[integer])) {
-                System.out.println("You are suggested to have a ICIC Lfe Insurance where it includes " + need + "\tinsurance");
-                break;
+        for (int integer = 0; integer < args.length; integer++) {
+            for (String each : starHealth) {
+                if (each.contains(args[integer])) {
+                    System.out.println("Star health provides " + args[integer]);
+                }
             }
-        }
-       for(int integer=0;integer<=myInsurance.length;integer++){
-           if(need.equals(myInsurance[integer])) {
-            System.out.println("You are suggested to have a My Insurance where it includes "+need+ "\tinsurance");
-            break;}
+            for (String each : icicLife) {
+                if (each.contains(args[integer])) {
+                    System.out.println("ICIC helath insurance provides " + args[integer]);
+                }
+            }
+            for (String each : myInsurance) {
+                if (each.contains(args[integer])) {
+                    System.out.println("My Insurance  provides " + args[integer]);
+                }
+            }
+
+
         }
     }
 }
