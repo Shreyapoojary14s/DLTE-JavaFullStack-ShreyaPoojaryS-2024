189f4846921477ba1b98457d218e8445e4074c5b
Shreyapoojary14s <siripoojary@gmail.com>
2024-03-08 23:17:20 +0530

    report

--- DLTE-Database/USERPREVILAGES.sql
+++ DLTE-Database/USERPREVILAGES.sql
@@ -1,10 +1,8 @@
 
 SQL> create user Asha identified by asha1234;
-
 User created.
 
 SQL> grant select on transactionsanalysis to asha;
-
 Grant succeeded.
 
 
@@ -13,69 +11,54 @@ Grant succeeded
 
 SQL> alter session set current_schema=system;
 
-//delete
+--delete
 SQL> create user SEETHARAM identified by seetha1234 ;
-
 User created.
 
 SQL> grant delete on transactionsanalysis to seetha;
-
 Grant succeeded.
 
 SQL> grant connect to SEETHARAM;
 Grant succeeded.
 
 SQL> alter session set current_schema=system;
-
 Session altered.
 
 --GRANT 
 SQL> create user Harith identified by shreya123;
-
 User created.
 
 SQL> grant select on transactionsanalysis to shreya;
-
 Grant succeeded.
 
 SQL> grant connect to shreya;
-
 Grant succeeded.
 
 SQL> alter session set current_schema=system;
-
 Session altered.
 
 -------SIRI
 SQL> create user siri identified by siri1234;
-
 User created.
 
 SQL> grant insert on transactionsanalysis to siri;
-
 Grant succeeded.
 
 SQL> grant connect to siri;
-
 Grant succeeded.
 
 SQL> alter session set current_schema=system;
-
 Session altered.
 
 
 SQL> create user nandu identified by siri1234;
-
 User created.
 
 SQL> grant update on transactionsanalysis to siri;
-
 Grant succeeded.
 
 SQL> grant connect to siri;
-
 Grant succeeded.
 
 SQL> alter session set current_schema=system;
-
 Session altered.
