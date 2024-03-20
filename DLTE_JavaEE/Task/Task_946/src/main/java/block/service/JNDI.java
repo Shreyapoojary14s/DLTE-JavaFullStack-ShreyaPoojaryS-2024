2cf5b46ac1c340421f2c1580050ff75041e3c337
Shreyapoojary14s <siripoojary@gmail.com>
2024-03-20 17:57:35 +0530

    updated

--- DLTE_JavaEE/Task/Task_946/src/main/java/block/service/JNDI.java
+++ DLTE_JavaEE/Task/Task_946/src/main/java/block/service/JNDI.java
@@ -1,7 +1,9 @@
 package block.service;
+//Update data in account
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,44 +13,45 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+
 import java.sql.SQLException;
 
-    @WebServlet("/app/updateAccount/*")
-    public class JNDI extends HttpServlet {
-        @Override
-        protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            Connection connection = null;
-            PreparedStatement preparedStatement = null;
-            try {
-                Context context = new InitialContext();
-                DataSource dataSource = (DataSource) context.lookup("java:/OracleDS");
-                connection = dataSource.getConnection();
-                String requestUser = req.getParameter("user");
-                String requestPassword = req.getParameter("password");
-                if (requestUser != null && requestPassword != null) {
-                    String sql = "UPDATE Account SET password = ? WHERE username = ?";
-                    preparedStatement = connection.prepareStatement(sql);
-                    preparedStatement.setString(1, requestPassword);
-                    preparedStatement.setString(2, requestUser);
-                    int rowsAffected = preparedStatement.executeUpdate();
-                    if (rowsAffected == 1) {
-                        System.out.println("Account has been updated");
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (preparedStatement != null) {
-                        preparedStatement.close();
-                    }
-                    if (connection != null) {
-                        connection.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+@WebServlet("/updateData/")
+class UpdateServlet extends HttpServlet {
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Context context= new InitialContext();
+            Connection connection=null;
+            PreparedStatement preparedStatement;
 
+            //default in name: java:/OracleDS
+            DataSource dataSource = (DataSource) context.lookup("java:/OracleDS");
+
+            // Get a connection from the DataSource
+            connection = dataSource.getConnection();
+
+            //user and password parameters from the request
+            String username=req.getParameter("user");
+            String password = req.getParameter("password");
+            if(username !=null && password !=null){
+
+                // update the password for the given username
+                String query = "update account set password=? where username=?";
+                preparedStatement=connection.prepareStatement(query);
+                preparedStatement.setString(1,password);
+                preparedStatement.setString(2,username);
+                int updatesRows=preparedStatement.executeUpdate();
+                
+                // Check if the update was successful
+                if (updatesRows==1)
+                    resp.getWriter().println("Account updated");
+                else
+                    resp.getWriter().println("Failed to update");
+            }
+        }catch (NamingException | SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
+
