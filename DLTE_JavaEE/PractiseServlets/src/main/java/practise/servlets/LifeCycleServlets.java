ab6caed7102e0cfd8720f1e0b7df96d985e82e97
Shreyapoojary14s <siripoojary@gmail.com>
2024-03-21 10:17:50 +0530

    updated

---
+++ DLTE_JavaEE/PractiseServlets/src/main/java/practise/servlets/LifeCycleServlets.java
@@ -0,0 +1,33 @@
+package practise.servlets;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/obj")
+public class LifeCycleServlets extends HttpServlet{
+    // Initialization method
+    public void init() throws ServletException {
+        System.out.println("Servlet initialized");
+    }
+
+    // Request handling method (GET)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Hello Servlet</title></head>");
+        out.println("<body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+
+    // Cleanup method
+    public void destroy() {
+        System.out.println("Servlet destroyed");
+    }
+
+}
