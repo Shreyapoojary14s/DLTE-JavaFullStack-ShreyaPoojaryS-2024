6cf943bd4de10ddfd490155b6543a54292458cea
Shreya S Poojary <96349610+Shreyapoojary14s@users.noreply.github.com>
2024-04-01 20:17:18 +0530

    Update and rename MyBankOfficials.java to MyBankOfficialsEntity.java

--- DLTE-Spring/Task/RestSecurityAuth/src/main/java/springjdbc/dao/demo/security/MyBankOfficialsEntity.java
+++ DLTE-Spring/Task/RestSecurityAuth/src/main/java/springjdbc/dao/demo/security/MyBankOfficialsEntity.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class MyBankOfficials implements UserDetails {
+public class MyBankOfficialsEntity implements UserDetails {
     private String name;
     private String username;
     private String password;
