16954e52941e114103758f2daae1f194eec1d2b2
Shreya S Poojary <96349610+Shreyapoojary14s@users.noreply.github.com>
2024-04-01 20:14:11 +0530

    Update TransactSecurity.java

--- DLTE-Spring/Task/RestSecurityAuth/src/main/java/springjdbc/dao/demo/security/TransactSecurity.java
+++ DLTE-Spring/Task/RestSecurityAuth/src/main/java/springjdbc/dao/demo/security/TransactSecurity.java
@@ -29,14 +29,21 @@ public class TransactSecurity {
         httpSecurity.formLogin();
         httpSecurity.csrf().disable();
 
+        //authority >>ROLES >>who can access
         httpSecurity.authorizeRequests().antMatchers("/profile/addTransaction").permitAll();
-
+        //sender>>access to >>customer
         httpSecurity.authorizeRequests().antMatchers("/transaction/Sender/*").hasAuthority("customer");
-        httpSecurity.authorizeRequests().antMatchers("/transaction/Receiver/*").hasAuthority("customer");
-        httpSecurity.authorizeRequests().antMatchers("/transaction/Amount/*").hasAuthority("customer");
+         //receiver>>access to >>cashier
+        httpSecurity.authorizeRequests().antMatchers("/transaction/Receiver/*").hasAuthority("cashier");
+         //amount>>access to >>cashier
+        httpSecurity.authorizeRequests().antMatchers("/transaction/Amount/*").hasAuthority("cashier");
+         //update>>access to >>manager & admin
         httpSecurity.authorizeRequests().antMatchers("/transaction/Update/*").hasAnyAuthority("manager","admin");
+         //post>>access to >>admin
         httpSecurity.authorizeRequests().antMatchers(HttpMethod.POST).hasAuthority("admin");
+          //delete>>access to >>admin
         httpSecurity.authorizeRequests().antMatchers(HttpMethod.DELETE).hasAuthority("admin");
+            //put>>access to >>manager & admin
         httpSecurity.authorizeRequests().antMatchers(HttpMethod.PUT).hasAnyAuthority("manager","admin");
 
         httpSecurity.authorizeRequests().anyRequest().authenticated();
