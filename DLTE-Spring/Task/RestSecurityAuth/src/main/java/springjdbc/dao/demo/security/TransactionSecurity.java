5ed02e9a873a49ed5a3aa1f5e3ae91962b78647c
Shreya S Poojary <96349610+Shreyapoojary14s@users.noreply.github.com>
2024-04-01 20:18:25 +0530

    Update and rename TransactSecurity.java to TransactionSecurity.java

--- DLTE-Spring/Task/RestSecurityAuth/src/main/java/springjdbc/dao/demo/security/TransactionSecurity.java
+++ DLTE-Spring/Task/RestSecurityAuth/src/main/java/springjdbc/dao/demo/security/TransactionSecurity.java
@@ -12,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class TransactSecurity {
+public class TransactionSecurity {
     @Autowired
   MyBankOfficialService myBankOfficialService;
 
