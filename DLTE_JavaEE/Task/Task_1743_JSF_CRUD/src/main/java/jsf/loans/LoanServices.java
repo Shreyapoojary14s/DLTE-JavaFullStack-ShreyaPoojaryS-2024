ed572bed96fdd0bde92ce486177953eb2001db6a
Shreya S Poojary <96349610+Shreyapoojary14s@users.noreply.github.com>
2024-04-28 19:48:48 +0530

    Update LoanServices.java

--- DLTE_JavaEE/Task/Task_1743_JSF_CRUD/src/main/java/jsf/loans/LoanServices.java
+++ DLTE_JavaEE/Task/Task_1743_JSF_CRUD/src/main/java/jsf/loans/LoanServices.java
@@ -20,6 +20,22 @@ public class LoanServices {
     public void setLoans(List<Loans> loans) {
         this.loans = loans;
     }
+      //Adding the new loan
+    public void createLoans(Loans loans1){
+        try {
+            loans.add(loans1);
+            System.out.println("New loan insertion successfull!!");
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
+    }
+        //Display closed loans
+    public List<Loans> closedLoans(){
+        List<Loans> filteredLoans=loans.stream().filter(loans1->loans1.getLoanStatus().equals("closed")).collect(Collectors.toList());
+        filteredLoans.forEach(System.out::println);
+        return filteredLoans;
+    }
+
 
     @PostConstruct
     public void init(){
@@ -31,28 +47,35 @@ public class LoanServices {
         loans.addAll(Stream.of(loanlist).collect(Collectors.toList()));
     }
 
-    //method corresponding to adding the new loan
-    public void createLoans(Loans loans1){
-        try {
-            loans.add(loans1);
-            System.out.println("New loan insertion successfull!!");
-        }catch (Exception ex){
-            System.out.println(ex);
-        }
-    }
-
-
-    //method corresponds to the deletion of loans based on loan number
+  
+    //Deletion of loans 
     public void removeLoan(long loanNumber){
         loans.removeIf(loans->loans.getLoanNumber().equals(loanNumber));
         System.out.println("Deletion successful!!");
     }
 
-    //method to display the closed loans
-    public List<Loans> closedLoans(){
-        List<Loans> filteredLoans=loans.stream().filter(loans1->loans1.getLoanStatus().equals("closed")).collect(Collectors.toList());
-        filteredLoans.forEach(System.out::println);
-        return filteredLoans;
-    }
+    // public List<Loans> closedLoans(){
+    //     List<Loans> filteredLoans=loans.stream().filter(loans1->loans1.getLoanStatus().equals("closed")).collect(Collectors.toList());
+    //     filteredLoans.forEach(System.out::println);
+    //     return filteredLoans;
+    // }
+
+
+
+    // public void init(){
+    //     Loans[] loanlist={
+    //             new Loans(12345L,100.0,"10/04/2024","closed","pranav",7865787678L),
+    //             new Loans(54321L,2000.0,"21/03/2024","open","Raksha",9087678987L),
+    //             new Loans(67890L,3000.0,"15/02/2024","closed","venkatesh",7867898789L),
+    //     };
+    //     loans.addAll(Stream.of(loanlist).collect(Collectors.toList()));
+    // }
+
+  
+
+    // public void removeLoan(long loanNumber){
+    //     System.out.println("Deletion successful!!");
+    // }
+
 
 }
